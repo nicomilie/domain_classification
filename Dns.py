bb0ec217134c045af66f4f8f2ef78162a3cfd0ae
nico1000ie@hotmail.com <nico1000ie@hotmail.com>
2016-02-16 16:24:00 +0200

    Dns queries

--- Dns.py
+++ Dns.py
@@ -1,3 +1,15 @@
+import socket
+import dns.resolver
+import dns.query
+import dns.zone
 
-print("Hello world")
-print(2)
+import dns.resolver #import the module
+myResolver = dns.resolver.Resolver() #create a new instance named 'myResolver'
+web_site = raw_input("\nPlease enter a domain name that you wish to examinate   : ")
+
+myAnswers = myResolver.query(web_site, "A") #Lookup the 'A' record(s) for ... .com
+for rdata in myAnswers: #for each response
+    print rdata #print the
+    print "TTL: " + str(myAnswers.rrset.ttl)
+    print("")
+
---
+++ WhoIs.py
@@ -0,0 +1,5 @@
+import whois
+
+w = whois.whois('www.ovaciondigital.com')
+print w.expiration_date
+print w
